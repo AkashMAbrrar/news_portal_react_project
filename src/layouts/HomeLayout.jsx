5b27d8d86b0ada2ebada396b688113885ac67428
AkashMAbrrar <madborakash48@gmail.com>
2024-11-14 17:44:01 +0600

    this commit for left aside bar for loading data based on categories

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import NavBar from "../components/NavBar";
+import LeftNavbar from "../components/Layout_component/LeftNavbar";
 
 const HomeLayout = () => {
   return (
@@ -11,12 +12,17 @@ const HomeLayout = () => {
         <section className="w-11/12 mx-auto">
           <LatestNews></LatestNews>
         </section>
-        <section className="w-11/12 mx-auto py-6">
-          <NavBar></NavBar>
-        </section>
       </header>
-      <nav></nav>
-      <main></main>
+      <nav className="w-11/12 mx-auto py-6">
+        <NavBar></NavBar>
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-3">
+          <LeftNavbar></LeftNavbar>
+        </aside>
+        <section className="col-span-6">Main Content</section>
+        <aside className="col-span-3">Right Bar</aside>
+      </main>
       <footer></footer>
       <h2>Home And Fortress</h2>
     </div>
