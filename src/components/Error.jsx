74ea08f5ce1c91368d0f7e479105260b6b768425
AkashMAbrrar <madborakash48@gmail.com>
2024-11-18 19:56:41 +0600

    this commit for page not found created

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Error = () => {
   return (
     <div>
-      <h2>Error 404</h2>
+      <h2 className="text-4xl font-semibold text-center">Error 404</h2>
+      <img
+        className="w-4/5"
+        src="https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1"
+        alt=""
+      />
     </div>
   );
 };
